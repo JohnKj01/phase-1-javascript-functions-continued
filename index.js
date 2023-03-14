cdec8984ac13be273e096b9d573f4fbb86d3942b
John <johnkaranjairungu@gmail.com>
2023-03-14 18:19:46 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+/*function saturdayFun(activity="This Saturday, I want to roller-skate!") {
+    console.log(activity);
+}
+if*/
 // code your solution here
+//else { console.log(activity) };
+function saturdayFun(someString = 'roller-skate'){
+    return `This Saturday, I want to ${someString}!`;
+}
+function mondayWork(someString = 'go to the office'){
+    return `This Monday, I will ${someString}.`;
+}
+function wrapAdjective(wrapper){
+    return function (someString){
+        return `You are ${wrapper}${someString}${wrapper}!`; 
+    }
+}
